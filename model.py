500c383d795ac79027a8e68483c825abc4e8b733
waverDeep <dandyleader@naver.com>
2021-04-07 17:14:11 +0900

    edit model -> use prelu

--- model.py
+++ model.py
@@ -1,66 +1,27 @@
+from typing import Any
+
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-# FC 모델 2계층 너무 성능이 좋지 않음.
-# 그리고 음수가 나오면 안되는데 음수가 나오는 경향이 생김
-# 음수를 나오지 못하게 할 무언가가 필요함 ( 후처리? )
+
 class Generator(nn.Module):
-    def __init__(self, z_dim, output_dim=2): # latent_dim
-        super(Generator, self).__init__()
-        self.gen = nn.Sequential(
-            nn.Linear(z_dim, 15),
-            nn.LeakyReLU(0.1),
-            nn.Linear(15, output_dim),
-            nn.Tanh(),
-        )
+    def __init__(self, latent_dim, output_dim=2):
+        self.linear01 = nn.Linear(latent_dim, 15)
+        self.prelu01 = nn.PReLU()
+        self.linear02 = nn.Linear(15, output_dim)
 
     def forward(self, x):
-        return self.gen(x)
+        out = self.prelu01(self.linear01(x))
+        return self.linear02(out)
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_features=2):
-        super(Discriminator, self).__init__()
-        self.disc = nn.Sequential(
-            nn.Linear(in_features, 25),
-            nn.LeakyReLU(0.1),
-            nn.Linear(25, 1),
-            nn.Sigmoid(),
-        )
+    def __init__(self, input_dim=2):
+        self.linear01 = nn.Linear(input_dim, 25)
+        self.prelu01 = nn.PReLU()
+        self.linear02 = nn.Linear(25, 1)
+        self.sigmoid01 = nn.Sigmoid()
 
     def forward(self, x):
-        return self.disc(x)
-
-
-class Generator_V2(nn.Module):
-    def __init__(self, z_dim, output_dim=2): # latent_dim
-        super(Generator_V2, self).__init__()
-        self.gen = nn.Sequential(
-            nn.Linear(z_dim, 1024),
-            nn.BatchNorm1d(1024),
-            nn.ReLU(),
-            nn.Linear(1024, output_dim),
-            nn.Tanh(),
-        )
-
-    def forward(self, x):
-        return self.gen(x)
-
-
-class Discriminator_V2(nn.Module):
-    def __init__(self, in_features=2):
-        super(Discriminator_V2, self).__init__()
-        self.disc = nn.Sequential(
-            nn.Linear(in_features, 1024),
-            nn.LeakyReLU(0.2),
-            nn.Linear(1024, 1),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x):
-        return self.disc(x)
-
-
-
-
+        out = self.prelu01(self.linear01(x))
+        return self.sigmoid01(self.linear02(out))
