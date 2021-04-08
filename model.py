781df0adfe489307f03dd9bdbd6f9e253b157b72
waverDeep <dandyleader@naver.com>
2021-04-08 21:34:04 +0900

    add activation choices

--- model.py
+++ model.py
@@ -5,14 +5,19 @@ import torch.nn as nn
 
 
 class Generator(nn.Module):
-    def __init__(self, latent_dim, output_dim=2):
+    def __init__(self, latent_dim, output_dim=2, choice_activation_func='PReLU'):
         super(Generator, self).__init__()
+        self.choice_activation_func = choice_activation_func
         self.linear01 = nn.Linear(latent_dim, 15)
         self.prelu01 = nn.PReLU()
+        self.relu01 = nn.ReLU()
         self.linear02 = nn.Linear(15, output_dim)
 
     def forward(self, x):
-        out = self.prelu01(self.linear01(x))
+        if self.choice_activation_func=='PReLU':
+            out = self.prelu01(self.linear01(x))
+        elif self.choice_activation_func=='ReLU':
+            out = self.relu01(self.linear01(x))
         return self.linear02(out)
 
 
